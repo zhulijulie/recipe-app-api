de1709d8c852f41bdfe491b855b0e831bbb9e189
Li Zhu <juliezhuli@outlook.com>
2022-07-28 17:25:07 -0700

    fix lint

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,40 @@
+"""
+Test custom Django management commands.
+"""
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+# the @patch decor mock BaseCommand's check method
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test commands."""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready."""
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        # check if the mocked "check" method is called
+        # once with the "default db" parameter
+        patched_check.assert_called_once_with(databases=['default'])
+
+    # mock the wait between checks, cuz don't want to make unit test slow
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError."""
+        # mock raise some exceptions: first 2 times return Psycopg2OpError then
+        # next 3 times OperationalError..then ready
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
+            [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
