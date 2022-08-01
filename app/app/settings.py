eecac75d40af30bf30c7ca9f921957b1ff84a737
Li Zhu <juliezhuli@outlook.com>
2022-08-01 12:34:27 -0700

    add user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST'), #need to match of the docker-compose.yml
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'), 
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
@@ -128,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,23 @@
+"""
+Tests for models.
+"""
+from django.test import TestCase
+# the get user model helper function is provided by Django in order to
+# get the default user model for the project.
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
