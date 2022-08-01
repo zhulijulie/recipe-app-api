fb68b0bd9a69749cd7aac71c62f2e2087d87fa0c
Li Zhu <juliezhuli@outlook.com>
2022-08-01 12:51:10 -0700

    add create superuser

--- app/core/models.py
+++ app/core/models.py
@@ -14,13 +14,24 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
         # extra fields can provide keyword arguments, any number of keyword arguments that
         # will be passed into our model.
         # This is useful when you define additional fields, for example, a name, you can pass it as an extra
         # field and that will be automatically created when the user model is created.
         # self.model will be the User model
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
