5b2889d7bbca1bd2db5fe81029541ade251c841e
Li Zhu <juliezhuli@outlook.com>
2022-08-04 12:52:11 -0700

    implement create user API and add unit tests

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,72 @@
+"""
+Tests for the user API.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+# allow to get the url from the view the name for
+CREATE_USER_URL = reverse('user:create')
+
+
+# helper func to creat an user before test
+# **params pass in a generic params, allow us to do is pass in any dictionary
+# that contains params that we're going to call straight into the user.
+# So it gives us the flexibility to add any parameters that we want to
+# the function call and we can pass them through to the user that we create.
+def create_user(**params):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(**params)
+
+
+# public test allows unauthenticated requests
+class PublicUserApiTests(TestCase):
+    """Test the public features of the user API."""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user_success(self):
+        """Test creating a user is successful."""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'testpass123',
+            'name': 'Test Name',
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(email=payload['email'])
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)  # hashed pwd is not returned
+
+    def test_user_with_email_exists_error(self):
+        """Test error returned if user with email exists."""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'testpass123',
+            'name': 'Test Name',
+        }
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short_error(self):
+        """Test an error is returned if password less than 5 chars."""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'pw',
+            'name': 'Test name',
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
---
+++ app/user/urls.py
@@ -0,0 +1,16 @@
+"""
+URL mappings for the user API.
+"""
+from django.urls import path
+
+from user import views
+
+
+app_name = 'user'
+
+# Django expects a natural function for this parameter here, we use the as_view method to
+# get that view function. That is the way that Django rest framework converts the corres
+# base view into the supported Django view.
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+]
