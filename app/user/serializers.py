1acd0e9838e20fe923678511dae4e2fa72237b69
Li Zhu <juliezhuli@outlook.com>
2022-08-04 14:07:32 -0700

    implement token api and add unit test

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,7 +1,12 @@
 """
 Serializers for the user API View.
 """
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+)
+# common syntax to do translation w django
+from django.utils.translation import gettext as _
 
 from rest_framework import serializers
 
@@ -19,3 +24,30 @@ class UserSerializer(serializers.ModelSerializer):
         # overwriting the create method so that we can call get use model objects
         # then pass in the already validated data from our sterilizer.
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user."""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(  # authentication function that comes built in with Django.
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+        if not user:
+            # the view will translate out to a http 400 bad request,
+            # and it will include the message that we defined here
+            msg = _('Unable to authenticate with provided credentials.')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
