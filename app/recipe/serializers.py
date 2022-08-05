73620326d2132c052ddfc7a734524bd5c85397fd
Li Zhu <juliezhuli@outlook.com>
2022-08-05 16:19:16 -0700

    implement create tag api and add unit tests

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -9,12 +9,23 @@ from core.models import (
 )
 
 
+# move to top since gonna be nested into recipe
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
+    tags = TagSerializer(many=True, required=False)  # optional
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
 
 
@@ -24,11 +35,26 @@ class RecipeDetailSerializer(RecipeSerializer):
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
 
+    # by default, nested serializers is read-only, we need to overridden
+    # to customize to be able to add tags
+    def create(self, validated_data):
+        """Create a recipe."""
+        # remove tags from validated data if exists
+        # if tag doesn't exist, use []
+        tags = validated_data.pop('tags', [])
+        # Recipe model expects tags to be assigned as a related field,
+        # so expects it to be created separately and added as a
+        # relationship to recipe from the manytomany field
+        recipe = Recipe.objects.create(**validated_data)
+        # The context is passed to the sterilizer by the view when
+        # you're using the sterilizer for that particular view.
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                # name=tag['name'],
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
 
-class TagSerializer(serializers.ModelSerializer):
-    """Serializer for tags."""
-
-    class Meta:
-        model = Tag
-        fields = ['id', 'name']
-        read_only_fields = ['id']
+        return recipe
