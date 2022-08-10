cbee4f9fef0411d9a7e6543329fb269465c9685f
Li Zhu <juliezhuli@outlook.com>
2022-08-10 15:08:48 -0700

    implement update recipe's ingredient and add unit tests

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -94,9 +94,15 @@ class RecipeDetailSerializer(RecipeSerializer):
     def update(self, instance, validated_data):
         """Update recipe."""
         tags = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
+        # allow an empty list to clear the list
+        # so explicitly use None here
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
